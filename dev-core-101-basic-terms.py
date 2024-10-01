982e6922be3e603f96e855bd6318079ebd5f8d4c
aladdin1011 <nurmakov2015@gmail.com>
2024-10-02 02:36:50 +0500

    mem

---
+++ dev-core-101-basic-terms.py
@@ -0,0 +1,19 @@
+gender = 'men'
+
+def men():
+    print("Ты мужчина")
+
+if gender == 'men':
+    men()
+
+i = 0
+for i in range(2):
+    men()
+
+x = 9
+
+result = x-3
+
+number = 15  # слушаю песню, а ты? 
+
+is_adult = True 
