ec6502ddf915bf080fbfe1cd14e876f8a665ce13
aladdin1011 <nurmakov2015@gmail.com>
2024-10-06 23:36:30 +0500

    code

---
+++ dev-core-101-exercises-python.py
@@ -0,0 +1,21 @@
+temp = float(input("Введите градус:"))
+print(f"Конвертация цельсий в фаренгейт: {temp+273.15}")
+
+num = int(input("Введите число:"))
+
+if num%2 == 0:
+    print("Четное число")#
+else:
+    print("Нечетное число")
+
+num1 = int(input("Введите обычное число:"))
+if num1 <= 1:
+    print(f"{num1} не является простым числом.")
+else:
+    flag = True
+    for i in range (2,int(num1**0.5)+1):
+        if num1% i == 0:
+            flag = False
+            break
+    if flag:
+        print(f"{num1} простое число")
