944452cde5e402df15f9b73aab1e6d2aee3ac219
aladdin1011 <nurmakov2015@gmail.com>
2024-10-27 18:28:14 +0500

    back 3

--- backend-101/lesson-3/my_app/main.py
+++ backend-101/lesson-3/my_app/main.py
@@ -3,3 +3,8 @@ from utils.helpers import greet
 if __name__ == "__main__":
     message = greet("Мир")
     print(message)
+
+from utils.helpers import start_day
+
+
+start_day()
--- backend-101/lesson-3/my_app/utils/helpers.py
+++ backend-101/lesson-3/my_app/utils/helpers.py
@@ -3,3 +3,21 @@
 def greet(name):
     return f"Привет,{name}!"
 
+def make_coffee():
+    print("Drink a coffee")
+
+def do_exercise():
+    print("Do a morningn exercises")
+
+def eat_breakfast():
+    print("Eat the breakfast")
+
+def brush_teeth():
+    print("Brush the teets")
+
+
+def start_day():
+    brush_teeth()
+    do_exercise()
+    eat_breakfast()
+    make_coffee()
