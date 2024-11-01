4b9506eebc2da75bd06e9f6e34b6ec1fd8f0c926
aladdin1011 <nurmakov2015@gmail.com>
2024-11-01 23:49:48 +0500

    test

---
+++ dev-core-102/task_2_data_structures.py
@@ -0,0 +1,8 @@
+name_list =  ["Zhas","Kuat","Anuar","Adlet"]
+
+user_info = {}
+
+def user_info_add(name,age,email):
+    name,age,email = input("Введите имя"),input("Введите возраст"),input("Введите почту")
+    user_info.update({"name"} : name)
+    user_info.update()
