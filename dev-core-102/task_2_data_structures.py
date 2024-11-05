8b1f3883879213440b859fcccfc3ff2e245961cf
aladdin1011 <nurmakov2015@gmail.com>
2024-11-05 22:48:32 +0500

    task2

--- dev-core-102/task_2_data_structures.py
+++ dev-core-102/task_2_data_structures.py
@@ -1,45 +1,58 @@
-import os
-import json
+conference = ["Zhaskairat","Kuat","Adlet","Sanzar"]
+coordinates = (43,76)
+user_info = {
+    "Zhaskarat":{
+        "age":"20",
+        "email":"zhas@gmail.com"
+    },
+    "Kuat":{
+        "age":"19",
+        "email":"kuat@gmail.com"
+    },
+    "Adlet":{
+        "age":"20",
+        "email":"adlet@gmail.com"
+    },
+    "Sanzar":{
+        "age":"20",
+        "email":"sanzar@gmail.com"
+    }
+}
 
-file_path = "C:\\Users\\zhas\\Desktop\\full-stack-dev\\dev-core-102\\user_info.json"
-
-# Загружаем предыдущие данные, если файл уже существует
-
-if os.path.exists(file_path):
-    with open(file_path, "r") as file:
-        user_info = json.load(file)
-else:
-    user_info = []
-
-def user_info_add():
-    name = input("Введите имя: ")
-    age = input("Введите возраст: ")
-    email = input("Введите почту: ")
-
-    # Добавляем новый словарь с данными пользователя в список
-    user_info.append({"name": name, "age": age, "email": email})
-
-    # Сохраняем обновленные данные в файл
-    with open(file_path, "w") as file:
-        json.dump(user_info, file)
-    print("Данные сохранены!")
-
-
-def user_info_remove(index):
-    try:
-        remove_user = user_info.pop(index)
-        print(f"Пользователь{remove_user["name"]} удален.")
-
-        with open(file_path,'w') as f:
-            json.dump(user_info,f)
-        print("Данные обновлены и сохранены!")
-    except IndexError:
-        print("Некорретный имя пользователя")
+def add_user():
+    name = input("Введите имя пользователя: ")
+    if name in user_info:
+        print(f"Пользователь {name} уже существует")
+        return
+    age = input("Введите возраст пользователя: ")
+    email = input("Введите почту пользователя: ")
     
-user_info_add()
-print("Список пользователей: ",user_info)
+    user_info[name] = {
+        "age": age,
+        "email": email
+    }
+    print(f"Пользователь {name}, успешно добавлен")
 
-index_to_remove = int(input("Введите индекс пользователя для удаления: "))
+def remove_user():
+    name = input("Введите имя пользователя: ")
+    if name in user_info:
+        del user_info[name]
+        print(f"Пользователь удален {name}!")
+    else:
+        print(f"Пользователь {name}, не найден.")
 
-user_info_remove(index_to_remove)
-print("Обновленный список пользователей: " , user_info)
+def display_user():
+    name = input("Введите имя пользователя: ")
+    if name in user_info:
+        info = user_info[name]
+        print(f"Имя:{name}")
+        print(f"Возраст:{info['age']}")
+        print(f"Почта:{info["email"]}")
+    else:
+        print(f"Пользователь {name}, не найден")
+
+add_user()
+display_user()
+print(user_info)
+remove_user()
+print(user_info)
