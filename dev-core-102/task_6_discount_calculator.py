6e0b7b423ed82999de083dad4750caabb25dda19
aladdin1011 <nurmakov2015@gmail.com>
2024-11-09 13:34:55 +0500

    task6

---
+++ dev-core-102/task_6_discount_calculator.py
@@ -0,0 +1,36 @@
+def get_products():
+    print("Добро пожаловать!")
+    try:
+        quantity_products = int(input("Введите количество продуктов: "))
+    except ValueError:
+        print("Ошибка ввода, пожалуйста, введите целое число.")
+        return None
+    
+    prices = []
+    
+    print("Введите цену для каждого продукта (или введите 'стоп' для завершения ввода): ")
+
+    for i in range(quantity_products):
+        price_input = input(f"цена продукта {i + 1}: ")
+        if price_input.lower() == "стоп":
+            break
+        try:
+            price = float(price_input)
+            prices.append(price)
+        except ValueError:
+            print("Некорренктный ввод, пожалуйста, введите число.")
+            continue
+    return prices
+
+def  discount(prices):
+    discount_items = list(filter(lambda x: x>100,prices))
+    non_discount_prices = list(filter(lambda x: x<=100,prices))
+    discount_prices = list(map(lambda x: x*0.9,discount_items))
+    finish_prices = discount_prices + non_discount_prices 
+    total_sum = sum(finish_prices)
+    print("Цены со скидкой:", finish_prices)
+    print("Итоговая сумма с учётом скидок:", total_sum)
+def main():
+    prices = get_products()                   
+    discount(prices)
+main()
