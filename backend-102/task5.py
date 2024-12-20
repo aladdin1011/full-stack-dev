eb4f205358e0839f6ac76ee44d8be6ef84b3fa70
aladdin1011 <nurmakov2015@gmail.com>
2024-12-20 10:51:17 +0500

    Пофиксил зависимости, а так-же сделал хеширование пароля

--- backend-102/task5.py
+++ backend-102/task5.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, EmailStr, Field
+import bcrypt
 
 app = FastAPI()
 
 # Локальное хранилище пользователей
 users = [
-    {"id": 1, "name": "Adilet", "email": "adilet@example.com", "password": "password", "role": "admin"},
-    {"id": 2, "name": "Anuar", "email": "anuar@example.com", "password": "paswword", "role": "user"},
+    {"id": 1, "name": "Adilet", "email": "adilet@example.com", "password": bcrypt.hashpw("password".encode('utf-8'), bcrypt.gensalt()).decode('utf-8'), "role": "admin"},
+    {"id": 2, "name": "Anuar", "email": "anuar@example.com", "password": bcrypt.hashpw("paswword".encode('utf-8'), bcrypt.gensalt()).decode('utf-8'), "role": "user"},
 ]
-
 # Модель для регистрации пользователя
 class RegisterUser(BaseModel):
     name: str = Field(..., min_length=1, max_length=50)
@@ -26,13 +26,15 @@ def register_user(user: RegisterUser):
     # Проверить уникальность email
     if any(u["email"] == user.email for u in users):
         raise HTTPException(status_code=400, detail="Email already exists")
-
+    
+    #хэшированние пароля
+    hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
     # Создать нового пользователя
     new_user = {
         "id": len(users) + 1,
         "name": user.name,
         "email": user.email,
-        "password": user.password,  # Пароль пока хранится как обычный текст
+        "password": hashed_password,  # Пароль пока хранится как обычный текст
         "role": user.role,
     }
     users.append(new_user)
@@ -42,7 +44,12 @@ def register_user(user: RegisterUser):
 def login_user(credentials: LoginUser):
     # Найти пользователя по email
     user = next((u for u in users if u["email"] == credentials.email), None)
-    if not user or user["password"] != credentials.password:
-        raise HTTPException(status_code=401, detail="Invalid email or password")
+    if not user:
+        raise HTTPException(status_code=401,detail="Invalid emaill or password")
 
-    return {"message": f"Welcome, {user['name']}"}
+    # Проверка пароля
+    if not bcrypt.checkpw(credentials.password.encode('utf-8'), user["password"].encode('utf-8')):
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+    
+    return {"message": "Login successful!"}
+
